5b52dac1cbdd4b56e98d97555b14a0c79dfa3638
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-01-28 22:23:56 +0100

    Setup (vite, react & TailwindCSS)👍

---
+++ project-8/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+    <div className="bg-red-900 p-12">
+      hello
+    </div>
+    </div>
+  )
+}
+
+export default App
